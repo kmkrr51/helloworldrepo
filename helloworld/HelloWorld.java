cd5b911a4e272288aa22f0566ffee84ef3c2aeeb
kmkrr51 <133234620+kmkrr51@users.noreply.github.com>
2023-05-29 21:09:05 +0530

    Add files via upload

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+public class HelloWorld{
+
+
+public static void main(String args[]){
+	
+	printHello();
+	
+}
+
+public static String printHello(){
+
+System.out.println("Inside method printHello of HelloWorldSuccess!");
+
+return "HelloWorldSuccess";
+}
+
+}
